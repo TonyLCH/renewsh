73e2d61a2df7420b83a512afe47613057ee4ae06
TonyLCH <lchgamezone@gmail.com>
2022-05-02 14:00:44 +0800

    Update renews.sh

--- renews.sh
+++ renews.sh
@@ -1,3 +1,3 @@
 systemctl stop nginx
-asudo "$HOME/.acme.sh/acme.sh" --renew -d scvd.hkcls-network.cf --force -ecc
+sudo "$HOME/.acme.sh/acme.sh" --renew -d scvd.hkcls-network.cf --force -ecc
 systemctl start nginx
