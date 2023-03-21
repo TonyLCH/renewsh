f86ea36c3279821016e3722fa7cc5f3c842c30eb
TonyLCH <lchgamezone@gmail.com>
2023-03-21 22:02:07 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 systemctl stop nginx
-sudo "$HOME/.acme.sh/acme.sh" --renew -d www.hkcls-network.cf --force --ecc
+sudo "$HOME/.acme.sh/acme.sh" --renew -d web.hkcls-network.cf --force --ecc
 systemctl start nginx
 systemctl restart xray
