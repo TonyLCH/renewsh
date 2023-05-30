d056bd2ff9024ad59f84e82e5490ec4792fc8e90
ZeroDay4325 <lchgamezone@gmail.com>
2023-05-30 09:42:03 +0800

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,7 +1,7 @@
 echo 'deb http://deb.xanmod.org releases main' |tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | apt-key add -
 apt update && apt install linux-xanmod
-echo 'net.core.default_qdisc = fq_codel' | tee /etc/sysctl.d/90-override.conf
-echo "net.core.default_qdisc=fq_codel" | tee /etc/sysctl.d/99-sysctl.conf
+echo 'net.core.default_qdisc = fq' | tee /etc/sysctl.d/90-override.conf
+echo "net.core.default_qdisc=fq" | tee /etc/sysctl.d/99-sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr2" | tee /etc/sysctl.d/99-sysctl.conf
 echo "net.ipv4.tcp_retries2 = 8
 net.ipv4.tcp_slow_start_after_idle = 0
