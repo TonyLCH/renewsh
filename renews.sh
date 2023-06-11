8c267abee28eedb39a7374344fdaacb4c2f807c0
ZeroDay4325 <lchgamezone@gmail.com>
2023-06-11 19:34:18 +0800

    Update renews.sh

--- renews.sh
+++ renews.sh
@@ -1,4 +1,4 @@
 systemctl stop nginx
-sudo "$HOME/.acme.sh/acme.sh" --renew -d view.hkcls-network.cf --force --ecc
+sudo "$HOME/.acme.sh/acme.sh" --renew -d music.hkcls-network.cf --force --ecc
 systemctl start nginx
 systemctl restart xray
