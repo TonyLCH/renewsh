0827f1ecdda64faca93dccee76e6c9c9e8c2c7e5
ZeroDay4325 <lchgamezone@gmail.com>
2023-11-23 23:55:59 +0800

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,4 +1,4 @@
 wget -qO - https://dl.xanmod.org/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
-apt update && apt install linux-xanmod-x64v2
+apt update && apt install linux-xanmod-rt-x64v2
 apt autoremove
